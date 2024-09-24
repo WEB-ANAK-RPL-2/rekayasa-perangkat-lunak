6ee8b0a702eeb8608a17da110714162f980b2018
WEB-ANAK-RPL-2 <dn8048676@gmail.com>
2024-09-24 14:49:32 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+
+
+//Prints out the variable type
+// Gets user input
+var name = prompt("ASSALLAMMUALLAIKUM.WR.WB👏,SELAMAT DATANG DI WEB ANAK RPL-2, SILAHKAN MASUKAN NAMA KAMU DI SINI UNTUK MELANJUTKAN!");
+
+// Uses user input to print out information
+println("Hello " + name + "!");
+println(num + "?! That's my favorite number too!");
+
+// Prints out the variable type
+println("Name is a " + typeof name);
+println("Num is a " + typeof num);
